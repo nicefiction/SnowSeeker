5996a64240b133e7dd8375c0feb6afad3ba0d225
Olivier Van hamme <nicefiction@gmail.com>
2021-09-11 21:12:28 +0200

    Initial Commit

---
+++ Shared/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  Shared
+//
+//  Created by Olivier Van hamme on 11/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
